4a1fe988bc63d3997fda285724c74a8068594f36
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2024-09-03 16:33:28 +0530

    dynamic and explicit mapping for index filed datatypes in elasticsearch

---
+++ 7.Mapping_in_elasticsearch/1.Intro_mapping_elk.sh
@@ -0,0 +1,11 @@
+what is mapping in elasticsearch:
+
+Mapping is the process of defining how a document and the fields it contains are stored and indexed.
+
+
+Dynamic Mapping: Automatically detects the new fields and add them to the index mappings.
+This is useful when dealing with the unstructured data.
+
+Explicit Mapping: Provides detailed control over how each field in your document indexed and stored.
+
+
